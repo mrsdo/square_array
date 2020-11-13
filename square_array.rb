d8775d12403849d134ab42d8fb3bc0065aad462d
Martinique <me@martidolce.com>
2020-11-13 11:12:36 -0500

    commit tree a563dd2a3a0010b3aa7c2dc613130552394e7b10

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,21 @@
+require "pry"
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+
 def square_array(array)
-  # your code here
+  squared = []
+  # binding.pry
+
+  # array = [1,2,3]
+  # array.map do { |element| square_array(numbers) ** 2} end
+  # return square_array(numbers)
+
+  # array.map do |element|
+  #   square_array(numbers) ** 2
+  # end
+  # array.map { |element| element ** 2 }
+  array.each do  |element| squared << element ** 2
+  end
+  return squared
 end
